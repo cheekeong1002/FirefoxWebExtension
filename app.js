ec5a6c8e959a17fa2182979a7bfe56e9bb1e68dd
cheekeong1002 <p19011503@student.newinti.edu.my>
2022-07-04 22:38:23 +0800

    Added comments

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
+//set things to run when main function is called
 async function main(){
+    //slice the hashtag symbol in the variable passed from background.js file
+    //and store into a blocked scope variable called term
     let term = location.hash.slice(1);
 
+    //query selector will return the frame element in the index.html file
+    //and set the content of the frame with the Cambridge dictionary web page with the term searched
     document.querySelector("#dictionary-frame").src = "https://dictionary.cambridge.org/dictionary/english/" + term;
 }
 
--- background.js
+++ background.js
@@ -9,9 +9,12 @@ browser.contextMenus.create({
 browser.contextMenus.onClicked.addListener(contextMenuAction);
 
 function contextMenuAction(info, tab) {
+    //store the higlighted text in a blocked scope variable called term 
     let term = info.selectionText;
+    //set the sidebar's panel which defines the content of the sidebar
     browser.sidebarAction.setPanel({
+        //set index.html as content of sidebar and pass the variable to the html file
         panel: `/index.html#${term}`
     });
-    browser.sidebarAction.open();
+    browser.sidebarAction.open(); //displays the sidebar
 }
